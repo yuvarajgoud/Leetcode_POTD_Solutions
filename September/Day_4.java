728c34bed7c800977226e07a67da2cbd6cbeb3d1
yuvarajgoud <yuvarajgoud20@gmail.com>
2025-09-04 07:16:44 +0530

    Day_4 Solved

---
+++ September/Day_4.java
@@ -0,0 +1,9 @@
+// 3516. Find Closest Person
+
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int p1 = Math.abs(x-z);
+        int p2 = Math.abs(y-z);
+        return (p1 < p2) ? 1 : ((p1 > p2) ? 2 : 0);
+    }
+}
